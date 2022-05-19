afef914b4beba7a023197e2d6b568eefe4075f09
Bianca Kendall <[D)>
2022-05-19 15:02:34 +0200

    added requests on BookController class for INDEX, SHOW, POST and DELETE

---
+++ public_library/src/main/java/com/bnta/public_library/controllers/BookController.java
@@ -0,0 +1,49 @@
+package com.bnta.public_library.controllers;
+
+import com.bnta.public_library.models.Book;
+import com.bnta.public_library.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("books")
+public class BookController {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    // INDEX
+    @GetMapping
+    public ResponseEntity<List<Book>> getBooks() {
+        return new ResponseEntity(bookRepository.findAll(), HttpStatus.OK);
+    }
+
+    // SHOW
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBook(@PathVariable Long id) {
+        var found = bookRepository.findById(id);
+        return new ResponseEntity(found, found.isEmpty() ? HttpStatus.NOT_FOUND : HttpStatus.OK);
+    }
+
+    // POST
+    @PostMapping
+    public ResponseEntity<Book> createBook(@RequestBody Book newBook) {
+        bookRepository.save(newBook);
+        return new ResponseEntity(newBook, HttpStatus.CREATED);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<List<Book>> deleteBook(@PathVariable Long id) {
+        var found = bookRepository.findById(id);
+        bookRepository.deleteById(id);
+        return new ResponseEntity(bookRepository.findAll(), found.isEmpty() ? HttpStatus.NOT_FOUND : HttpStatus.ACCEPTED);
+    }
+
+
+
+}
